574c67b7db11c86505000a0500be6e6a5a06853e
John Bronson <jbronson@gmail.com>
2022-03-07 19:35:50 -0600

    both GET routes working properly

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
-const { Pool, Client} = require('pg')
-const pool = new Pool( {
+const { Pool, Client } = require('pg')
+const pool = new Pool({
   user: 'bronson',
   host: 'localhost',
   database: 'reviewsapi',
@@ -8,14 +8,80 @@ const pool = new Pool( {
 
 //pool.connect().then( () => console.log('hey we connected!')).catch( (error) => console.log('uh oh', error))
 
-const helloWorld = function() {
-  return pool.query('SELECT * from REVIEWS where ID=1')
+const getReviews = function (page, count, product_id) {
+  //need to build logic for page/count/sort
+
+  return pool.query(`SELECT json_build_object(
+    'product', product_id,
+    'page', ${page},
+    'count', ${count},
+    'results', (SELECT json_agg(
+      json_build_object(
+        'review_id', id,
+        'rating', rating,
+        'summary', summary,
+        'recommend', recommend,
+        'response', response,
+        'body', body,
+        'date', date,
+        'reviewer_name', reviewer_name,
+        'helpfulness', helpfulness,
+        'photos', (SELECT json_agg(json_build_object(
+          'id', reviews_photos.id,
+          'url', reviews_photos.url
+        )) FROM reviews_photos WHERE review_id = reviews.id))
+    ) as results FROM reviews WHERE product_id = ${product_id} )
+  ) FROM reviews WHERE product_id = ${product_id}`)
+
+
+  //return pool.query(`SELECT * from REVIEWS where ID < 10`)
 }
 
-// const client = new Client()
-// await client.connect()
-// const res = await
+const getMeta = function (product_id) {
+
+  //select name from characteristics where product_id=1
+
+//return pool.query(`SELECT row_to_json(row(name, id)) from characteristics where product_id=1`)
+
+  return pool.query(`SELECT json_build_object(
+    'product_id', ${product_id},
+    'ratings', json_build_object (
+      '1', (SELECT COUNT(rating) FROM reviews WHERE rating=1 AND product_id=$1),
+      '2', (SELECT COUNT(rating) FROM reviews WHERE rating=2 AND product_id=$1),
+      '3', (SELECT COUNT(rating) FROM reviews WHERE rating=3 AND product_id=$1),
+      '4', (SELECT COUNT(rating) FROM reviews WHERE rating=4 AND product_id=$1),
+      '5', (SELECT COUNT(rating) FROM reviews WHERE rating=5 AND product_id=$1)
+    ), 'recommended', json_build_object(
+      'true', (SELECT COUNT(recommend) FROM reviews WHERE recommend='true' AND product_id=$1),
+      'false', (SELECT COUNT(recommend) FROM reviews WHERE recommend='false' AND product_id=$1)
+    ), 'characteristics', (SELECT json_object_agg(characteristics.name, (SELECT json_build_object('id', characteristics.characteristic_id,
+    'value', (select avg(value)
+          FROM characteristic_reviews
+            WHERE characteristic_id=characteristics.characteristic_id))))
+              FROM characteristics
+                WHERE product_id=$1))
+     FROM reviews WHERE product_id = $1`,[product_id])
+
+
+    //json_object_agg will be used to put objects together but not in an array
+}
+
+
+
+/*
+select avg(value) from
+characteristic_reviews where
+characteristic_id = 5
+*/
 
 module.exports = {
-  helloWorld: helloWorld
+  getReviews: getReviews,
+  getMeta: getMeta
 }
+
+/* tables in the database:
+* reviews
+* characteristics
+* reviews_photos
+* characteristic_reviews
+*/
--- server/index.js
+++ server/index.js
@@ -3,17 +3,38 @@ const app = express()
 const port = 3000
 const db = require('../database/index.js')
 
+app.use(express.json())
+
 app.get('/', (req,res) => {
   res.send('Hello world!')
 })
 
-app.get('/reviews/', (req,res) => {
-  db.helloWorld()
+/* the /reviews/ route takes the following params:
+* page - integer
+* count - integer
+* sort - integer
+* product_id
+*/
+
+app.get('/reviews/', (req, res) => {
+  console.log('starting GET request at /reviews/')
+  db.getReviews(req.query.page, req.query.count, req.query.product_id)
   .then( (result) => {
-    console.log(result.rows[0])
-    res.send(result.rows[0].now)
+    res.status(200).send(result.rows[0].json_build_object)
   })
-  .catch( (error) => console.log('uh oh', error))
+  .catch( (error) => console.log('/reviews/ : uh oh:', error))
+})
+
+app.get('/reviews/meta/', (req, res) => {
+  console.log('starting GET request at /reviews/meta/')
+  console.log(req.query)
+
+  db.getMeta(req.query.product_id)
+  .then( (result) => {
+    res.status(200).send(result.rows[0].json_build_object)
+  })
+.catch( (error) => console.log)
+
 })
 
 app.listen(port, () => {
